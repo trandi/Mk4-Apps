f3fd385c76310c3ff0bccca4e5a80f0ace6ef87a
trandi <robotrandi@gmail.com>
2019-09-15 23:34:55 +0100

    RotaryPhone - Control the solenoid / bells.

--- rotary_phone/main.py
+++ rotary_phone/main.py
@@ -12,10 +12,11 @@ import ugfx
 import ugfx_helper
 from machine import Pin
 import utime
+from homescreen import *
 
 
-#sim800.poweron()
 
+##### DIALING #####
 dialing = False
 currentNumber = ""
 lastDigit = -1
@@ -30,22 +31,22 @@ timeLastPulse = utime.ticks_ms()
 '''
 def callbackDialStart(p):
     global pinDialStart, timeLastStart, timeLastPulse, dialing, currentNumber, lastDigit
-    currentTime = utime.ticks_ms()
+    currentTimeMs = utime.ticks_ms()
     # debounce
-    if(currentTime - timeLastStart > 300 and currentTime - timeLastPulse > 100):
+    if(currentTimeMs - timeLastStart > 300 and currentTimeMs - timeLastPulse > 100):
         if(pinDialStart.value() == 1 and dialing == False):
             dialing = True
             lastDigit = -1
-            timeLastStart = currentTime
-            print("Dial START:  /  " + str(currentTime))
+            timeLastStart = currentTimeMs
+            print("Dial START:  /  " + str(currentTimeMs))
         elif (pinDialStart.value() == 0 and dialing == True and lastDigit >= 0):
             dialing = False
             lastDigit = lastDigit + 1
             if(lastDigit >= 10):
                 lastDigit = lastDigit - 10
             currentNumber = currentNumber + str(lastDigit)
-            timeLastStart = currentTime
-            print("Dial STOP:  /  " + str(currentTime))
+            timeLastStart = currentTimeMs
+            print("Dial STOP:  /  " + str(currentTimeMs))
 '''        
     
 
@@ -66,38 +67,124 @@ def callbackDialPulse(p):
 pinDialPulse.irq(handler=callbackDialPulse)
 
 
-
-#pBell = Pin(Pin.GPIO_FET)
-#pBell.on()  # takes it to GND (whereas off() takes it to 3.3V)
-
-
-# initialize screen
-ugfx.init()
-ugfx.clear()
-# show text
-ugfx.text(5, 5, "NUMBER", ugfx.BLACK)
-
-# MAIN loop
-while True:
-    currentTime = utime.ticks_ms()
-
-    if(currentTime - timeLastStart > 300 and currentTime - timeLastPulse > 200):
+def updateDial(currentTimeMs):
+    global timeLastStart, dialing, currentNumber, lastDigit
+    if(currentTimeMs - timeLastStart > 300 and currentTimeMs - timeLastPulse > 200):
         if(pinDialStart.value() == 1 and dialing == False):
             dialing = True
             lastDigit = -1
-            timeLastStart = currentTime
-            print("Dial START:  /  " + str(currentTime))
+            timeLastStart = currentTimeMs
+            print("Dial START:  /  " + str(currentTimeMs))
         elif (pinDialStart.value() == 0 and dialing == True and lastDigit >= 0):
             dialing = False
             lastDigit = lastDigit + 1
             if(lastDigit >= 10):
                 lastDigit = lastDigit - 10
             currentNumber = currentNumber + str(lastDigit)
-            timeLastStart = currentTime
-            print("Dial STOP:  /  " + str(currentTime))
+            timeLastStart = currentTimeMs
+            print("Dial STOP:  /  " + str(currentTimeMs))
 
-    ugfx.text(5, 20, currentNumber, ugfx.RED)
-    #utime.sleep_ms(100)
+    ugfx.text(5, 60, currentNumber, ugfx.RED)
+
+
+##### RINGING #####
+pinBellEnable = Pin(Pin.GPIO_ETHLED1, Pin.OUT)
+pinBell = Pin(Pin.GPIO_ETHLED0, Pin.OUT)
+timeLastBellMove = 0
+timeLastFreqRefresh = 0
+bellFreqHz = 70
+ringBell = False
+
+
+# it would be so much easier to do this witn PWM or a Timer callback !
+# however the former is set on given pins and the latter doesn't work 
+def updateBell(currentTimeMs):
+    global ringBell, timeLastBellMove, pinBellEnable, pinBell, bellFreqHz
+    if ringBell:
+        pinBellEnable.on()
+        if (currentTimeMs - timeLastBellMove > (1000 / bellFreqHz)):
+            pinBell.value(not pinBell.value())
+            timeLastBellMove = currentTimeMs
+    else:
+        pinBellEnable.off()
+
+    global timeLastFreqRefresh
+    # change the frequency and update display rarely
+    if(currentTimeMs - timeLastFreqRefresh > 2000):
+        if(bellFreqHz > 50):
+            bellFreqHz = bellFreqHz - 40
+        else:
+            bellFreqHz = bellFreqHz + 40
+
+        ugfx.area(150, 5, 90, 20, ugfx.WHITE)
+        ugfx.text(150, 5, "Ring " + str(bellFreqHz) + "Hz" , ugfx.RED)
+        timeLastFreqRefresh = currentTimeMs
+    
+
+# Change freq of ring
+pinJoystickUp = Pin(Pin.GPIO_JOYU, Pin.IN) # PullUp, RisingEdge
+pinJoystickDown = Pin(Pin.GPIO_JOYD, Pin.IN) # PullUp, RisingEdge       
+
+def callbackJoystickUp(p):
+    global bellFreqHz
+    if(bellFreqHz < 80):
+        bellFreqHz = bellFreqHz + 1
+
+def callbackJoystickDown(p):
+    global bellFreqHz
+    if(bellFreqHz > 1):
+        bellFreqHz = bellFreqHz - 1
+    
+
+# external interrupts from the Joystick
+pinJoystickUp.irq(handler=callbackJoystickUp)
+pinJoystickDown.irq(handler=callbackJoystickDown)
+
+
+
+##### PHONE #####
+sim800.poweron()
+def updatePhone():
+    global ringBell
+    if sim800.isringing():
+        ringBell = True
+    else:
+        ringBell = False
+
+
+
+##### BATTERY INFO #####
+timeLastBatteryRefresh = 0
+def updateBattery(currentTimeMs):
+    global timeLastBatteryRefresh
+    if(currentTimeMs - timeLastBatteryRefresh > 10000):
+        ugfx.area(5, 5, 140, 20, ugfx.GRAY)
+        ugfx.text(5, 5, "Batt " + str(battery()) + "%" , ugfx.BLUE)
+        timeLastBatteryRefresh = currentTimeMs
+
+
+
+##### MAIN #####
+# initialize screen
+ugfx.init()
+ugfx.clear()
+ugfx.text(5, 30, "NUMBER", ugfx.BLACK)
+
+
+
+# MAIN loop
+while True:
+    currentTimeMs = utime.ticks_ms()
+
+    updateDial(currentTimeMs)
+
+    updateBell(currentTimeMs)
+
+    updatePhone()
+
+    updateBattery(currentTimeMs)
+    
+
 
 
 # closing
